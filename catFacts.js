efacb8a77eaa066447fef65d339fd07c9323ee34
Adam Metcalf <adam.s.metcalf@outlook.com>
2021-02-05 10:45:38 -0500

    added a maximum character limit to prevent text overflow (and excessive reading, I mean come on it's facts about cats people)

--- catFacts.js
+++ catFacts.js
@@ -5,17 +5,20 @@ let url;
 getFactBtn.addEventListener('click', fetchFact);
 
 function fetchFact(e) {
-  console.log(e);
+  // console.log(e);
   url = baseURL + '/facts/random?animal+type=cat&amount=1';
   console.log(url);
   fetch(url)
     .then(function(result){
       return result.json();
     }).then(function(json){
-      // some of the "facts" submitted by uses are really stupid or obvious placeholder text, but most of those are shorter than 18 characters. So:
+      // some of the "facts" submitted by uses are really stupid or obvious placeholder text, but most of those are shorter than 18 characters. I'd also like to limit the length of characters, so I don't run into overflow issues. So:
       if(json.text.length < 18){
-        console.log("not enough characters, this probably wasn't a very good fact anyway");
+        console.log("not enough characters, fetching a different fact");
         fetchFact();  // recursion!!! Squeeeee!
+      } else if (json.text.length > 300) {
+        console.log("too many characters, fetching a different fact");
+        fetchFact();
       } else {
       displayFact(json);
     }
@@ -25,8 +28,9 @@ function displayFact(json){
   while (factArea.firstChild){
     factArea.removeChild(factArea.firstChild);
   }
-  let text = json.text;
+    let text = json.text;
     let fact = document.createElement('p');
+    fact.className = 'factFill';
     fact.textContent = text;
     console.log(text);
     factArea.appendChild(fact);
