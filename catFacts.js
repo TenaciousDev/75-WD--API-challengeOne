c457a5f36da5f8f3700d4d72352a2cd8a70d0224
Adam Metcalf <adam.s.metcalf@outlook.com>
2021-02-06 16:17:44 -0500

    added artwork of cat

--- catFacts.js
+++ catFacts.js
@@ -1,45 +1,46 @@
-const baseURL = 'https://cat-fact.herokuapp.com';
-const factArea = document.querySelector('.fact-area');
-const getFactBtn = document.querySelector('.feed');
+const baseURL = "https://cat-fact.herokuapp.com";
+const factArea = document.querySelector(".fact-area");
+const getFactBtn = document.querySelector(".feed");
 let url;
-getFactBtn.addEventListener('click', fetchFact);
+getFactBtn.addEventListener("click", fetchFact);
 
 function fetchFact(e) {
   // console.log(e);
-  url = baseURL + '/facts/random?animal+type=cat&amount=1';
+  url = baseURL + "/facts/random?animal+type=cat&amount=1";
   console.log(url);
   fetch(url)
-    .then(function(result){
+    .then(function (result) {
       return result.json();
-    }).then(function(json){
+    })
+    .then(function (json) {
       // some of the "facts" submitted by uses are really stupid or obvious placeholder text, but most of those are shorter than 18 characters. I'd also like to limit the length of characters, so I don't run into overflow issues. So:
-      if(json.text.length < 18){
+      if (json.text.length < 18) {
         console.log("not enough characters, fetching a different fact");
-        fetchFact();  // recursion!!! Squeeeee!
+        fetchFact(); // recursion!!! Squeeeee!
       } else if (json.text.length > 300) {
         console.log("too many characters, fetching a different fact");
         fetchFact();
       } else {
-      displayFact(json);
-    }
-    })
+        displayFact(json);
+      }
+    });
 
-function displayFact(json){
-  while (factArea.firstChild){
-    factArea.removeChild(factArea.firstChild);
-  }
+  function displayFact(json) {
+    while (factArea.firstChild) {
+      factArea.removeChild(factArea.firstChild);
+    }
     let text = json.text;
-    let fact = document.createElement('p');
-    fact.className = 'fact-bubble';
+    let fact = document.createElement("p");
+    fact.className = "fact-bubble";
     fact.textContent = text;
-    let factChars = text.length
+    let factChars = text.length;
     let factStyle = fact.style;
-    if(factChars < 80){
-      factStyle.width = text.length/2 + 'ch';
-      factStyle.padding = '5px';
+    if (factChars < 80) {
+      factStyle.width = text.length / 2 + "ch";
+      factStyle.padding = "5px";
     } else {
-      factStyle = text.length/3 + 'ch';
-      factStyle.padding = '5px';
+      factStyle = text.length / 3 + "ch";
+      factStyle.padding = "5px";
     }
     console.log(text);
     factArea.appendChild(fact);
